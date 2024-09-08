30c0e43f4d36283400ee8ba958e077032c4c8d74
thiwanka570 <jvptmadushan@gmail.com>
2024-09-08 23:57:08 +0530

    Signed-off-by: thiwanka570 <jvptmadushan@gmail.com>

--- order/src/main/java/com/orderservice/order/repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.orderservice.order.repository;
-
-import com.orderservice.order.entity.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
