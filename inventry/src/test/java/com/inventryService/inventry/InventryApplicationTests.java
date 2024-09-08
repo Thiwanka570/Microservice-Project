2a8c8d3bd1eceaf629679d14b0064fe7da9bb43c
thiwanka570 <jvptmadushan@gmail.com>
2024-09-08 20:52:19 +0530

    Signed-off-by: thiwanka570 <jvptmadushan@gmail.com>

---
+++ inventry/src/test/java/com/inventryService/inventry/InventryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inventryService.inventry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
